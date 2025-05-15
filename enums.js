6d58e516374b90ff04b4e792186749261972f15f
ttetrafon <info@ttetrafon.com>
2025-05-16 00:07:25 +0300

    split enums between local & library

---
+++ enums.js
@@ -0,0 +1,32 @@
+import { editablePageOperationNamesLocal, eventNamesLocal, generalNamesLocal, messageTypesLocal } from "../data/enums";
+
+const eventNamesLib = {
+  DIALOG_OPEN: Symbol("open-dialog"),
+  DIALOG_CANCEL: Symbol("cancel-dialog"),
+  DIALOG_CONFIRM: Symbol("confirm-dialog"),
+  NAVIGATE: Symbol("navigate"),
+  SUB_PAGE_CONTAINER: Symbol("sub-page-container"),
+  TOGGLE_SPINNING_CIRCLE: Symbol("toggle-spinning-circle")
+};
+export const eventNames = Object.freeze({ ...eventNamesLib, ...eventNamesLocal });
+
+const generalNamesLib = {
+  OBSERVABLE_USER: Symbol("user"),
+
+  CONNECTION_LIVE: Symbol("connection-live"),
+  CONNECTION_SOLO: Symbol("connection-solo"),
+  CONNECTION_OFFLINE: Symbol("connection-offline")
+};
+export const generalNames = Object.freeze({ ...generalNamesLib, ...generalNamesLocal });
+
+const editablePageOperationNamesLib = {
+  LINE_ADDED: Symbol("new-line"),
+  LINE_DELETED: Symbol("delete-line"),
+  LINE_UPDATED: Symbol("update-line")
+};
+export const editablePageOperationNames = Object.freeze({ ...editablePageOperationNamesLib, ...editablePageOperationNamesLocal });
+
+const messageTypesLib = {
+
+};
+export const messageTypes = Object.freeze({ ...messageTypesLib, ...messageTypesLocal });
